db4bb5cbb7f64246d221ebe07f48f86980de4d24
Nikolay Gavrilov <55625854+master-kot@users.noreply.github.com>
2020-10-16 17:09:59 +0300

    Update UserRequest.java

--- src/main/java/ru/dosport/dto/UserRequest.java
+++ src/main/java/ru/dosport/dto/UserRequest.java
@@ -2,12 +2,8 @@ package ru.dosport.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import static ru.dosport.entities.Messages.*;
@@ -15,15 +11,12 @@ import static ru.dosport.entities.Messages.*;
 /**
  * Запрос для регистрации нового Пользователя
  */
-@Setter
-@Getter
-@NoArgsConstructor
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserRequest {
 
     @Size(min=4, max=50, message = INVALID_USERNAME_LENGTH)
     @NotBlank(message = USERNAME_NOT_BLANK)
-//    @Email(message = EMAIL_FORMAT_ERROR)
     private String username;
 
     private String firstName;
