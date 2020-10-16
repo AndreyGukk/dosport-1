070819994d6e116ce86089d40b61a272bb67c113
Nikolay Gavrilov <55625854+master-kot@users.noreply.github.com>
2020-10-16 17:31:14 +0300

    Update User_Calendar.java

--- src/main/java/ru/dosport/future/User_Calendar.java
+++ src/main/java/ru/dosport/future/User_Calendar.java
@@ -1,7 +1,8 @@
 package ru.dosport.future;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.dosport.entities.User;
 
 import javax.persistence.*;
@@ -9,22 +10,22 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
-import static javax.persistence.FetchType.EAGER;
-
 /**
  * Сущность календарь пользователя
  */
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "user_calendar")
 public class User_Calendar {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    // чей календарь
+    // Пользователь - владелец календаря
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
@@ -32,5 +33,4 @@ public class User_Calendar {
     @ManyToMany(fetch = EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "event_id")// Внешний ключ
     private Map<LocalDate, Event> kindsOfSport = new HashMap<>();
-
 }
