25ff10e6efb43a28993c093052b479bfb146b58f
Milkky <Milkky598@yandex.ru>
2020-11-17 18:58:01 +0500

    Swagger & JavaDoc

--- src/main/java/ru/dosport/controllers/ReviewController.java
+++ src/main/java/ru/dosport/controllers/ReviewController.java
@@ -5,32 +5,50 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import ru.dosport.dto.ErrorDto;
 import ru.dosport.dto.ReviewDto;
 import ru.dosport.dto.ReviewRequest;
 import ru.dosport.services.api.ReviewService;
 
+import javax.validation.Valid;
 import java.util.List;
 
+import static ru.dosport.helpers.Messages.*;
 import static ru.dosport.helpers.Roles.ROLE_ADMIN;
 import static ru.dosport.helpers.Roles.ROLE_USER;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/sportgrounds", produces = "application/json")
+@Api(tags = {"Контроллер Отзывов пользователя"})
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     @GetMapping("/{sportGroundsId}/reviews/{reviewId}")
+    @ApiOperation(value = "Отображает отзыв по индентификатору")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = SUCCESSFUL_REQUEST),
+            @ApiResponse(code = 400, message = BAD_REQUEST, response = ErrorDto.class),
+            @ApiResponse(code = 404, message = DATA_NOT_FOUND, response = ErrorDto.class),
+    })
     public ResponseEntity<ReviewDto> getReview(@PathVariable Long sportGroundsId, @PathVariable Long reviewId) {
         return reviewService.readReviewDtoById(reviewId, sportGroundsId);
     }
 
+    @ApiOperation(value = "Отображенеи отзывы спортивной площадки")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = SUCCESSFUL_REQUEST)})
     @GetMapping("/{sportGroundsId}/reviews")
     public ResponseEntity<List<ReviewDto>> getAllReviews(@PathVariable Long sportGroundsId) {
         return reviewService.readAllReviewsDtoBySportGround(sportGroundsId);
     }
 
+    @ApiOperation(value = "Создает новый отзыв пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = SUCCESSFUL_REQUEST),
+            @ApiResponse(code = 400, message = BAD_REQUEST, response = ErrorDto.class),
+            @ApiResponse(code = 404, message = DATA_NOT_FOUND, response = ErrorDto.class)
+    })
     @Secured(value = {ROLE_USER, ROLE_ADMIN})
     @PostMapping("/{sportGroundsId}/review")
     public ResponseEntity<ReviewDto> createReview(@PathVariable Long sportGroundsId, @RequestBody ReviewRequest request,
@@ -38,6 +56,12 @@ public class ReviewController {
         return reviewService.saveReview(sportGroundsId, request, authentication);
     }
 
+    @ApiOperation(value = "Редактирует отзыв пользователя. Редактировать может только сам пользователь")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = SUCCESSFUL_REQUEST),
+            @ApiResponse(code = 400, message = BAD_REQUEST, response = ErrorDto.class),
+            @ApiResponse(code = 404, message = DATA_NOT_FOUND, response = ErrorDto.class)
+    })
     @Secured(value = {ROLE_USER, ROLE_ADMIN})
     @PutMapping("/{sportGroundsId}/review/{reviewId}")
     public ResponseEntity<ReviewDto> updateReview(@PathVariable Long sportGroundsId, @PathVariable Long reviewId,
@@ -45,6 +69,12 @@ public class ReviewController {
         return reviewService.updateReview(sportGroundsId, reviewId, request, authentication);
     }
 
+    @ApiOperation(value = "Удаляет отзыв пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = SUCCESSFUL_REQUEST),
+            @ApiResponse(code = 400, message = BAD_REQUEST, response = ErrorDto.class),
+            @ApiResponse(code = 404, message = DATA_NOT_FOUND, response = ErrorDto.class)
+    })
     @Secured(value = {ROLE_USER, ROLE_ADMIN})
     @DeleteMapping("/{sportGroundsId}/review/{reviewId}")
     public ResponseEntity<?> deleteReview(@PathVariable Long sportGroundsId, @PathVariable Long reviewId,
--- src/main/java/ru/dosport/services/api/ReviewService.java
+++ src/main/java/ru/dosport/services/api/ReviewService.java
@@ -8,13 +8,46 @@ import java.util.List;
 
 public interface ReviewService {
 
+    /**
+     * Возращает рецензию по её идентификатору
+     * @param reviewId идентификатор рецензии
+     * @param sportGroundId идентификатор площадки
+     * @return ResponseEntity<ReviewDto>
+     */
     ResponseEntity<ReviewDto> readReviewDtoById(Long reviewId, Long sportGroundId);
 
+    /**
+     * Возращает рецензии по идентификатору площадки
+     * @param sportGroundId идентификатор площадки
+     * @return ResponseEntity<List<ReviewDto>>
+     */
     ResponseEntity<List<ReviewDto>> readAllReviewsDtoBySportGround(Long sportGroundId);
 
+    /**
+     * Сохраняет новую рецензию
+     * @param sportGroundId идентификатор площадки
+     * @param request запрос (текст рецензии)
+     * @param authentication данные авторизации
+     * @return ResponseEntity<ReviewDto>
+     */
     ResponseEntity<ReviewDto> saveReview(Long sportGroundId, ReviewRequest request, Authentication authentication);
 
+    /**
+     * Обновляет рецензию (текст, имя юзера)
+     * @param reviewId идентификатор рецензии
+     * @param sportGroundId идентификатор площадки
+     * @param request запрос (текст рецензии)
+     * @param authentication данные авторизации
+     * @return ResponseEntity<ReviewDto>
+     */
     ResponseEntity<ReviewDto> updateReview(Long reviewId, Long sportGroundId, ReviewRequest request, Authentication authentication);
 
+    /**
+     * Удаление рецензии (только автор рецензии)
+     * @param reviewId идентификатор рецензии
+     * @param sportGroundId идентификатор площадки
+     * @param authentication данные авторизации
+     * @return ResponseEntity.ok/badRequest/noContent. Удаление произошло успешно/возникли ошибки/ такой сущности нет
+     */
     ResponseEntity<?> deleteById(Long reviewId, Long sportGroundId, Authentication authentication);
 }
